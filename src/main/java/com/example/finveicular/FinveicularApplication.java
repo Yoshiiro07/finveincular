a2a7829fd84509e77cfd824fd530d0ee201a6aa6
Gui Oliveira <59020911+Yoshiiro07@users.noreply.github.com>
2025-03-17 09:56:48 -0300

    Update start project
    Update start project

---
+++ src/main/java/com/example/finveicular/FinveicularApplication.java
@@ -0,0 +1,13 @@
+package com.example.finveicular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinveicularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinveicularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/finveicular/FinveicularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finveicular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinveicularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
